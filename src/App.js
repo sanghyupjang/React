bf9e77857cb6b1f1d4dd6351f6e0b44e6728edf6
Sanghyup <sanghyeob93@naver.com>
2022-02-12 14:22:03 +0900

    push

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
+import Button from "./Button";
+import styles from "./App.module.css";
 
 function App() {
   return (
     <div>
       <h1>Welcome back!!!</h1>
-      </div>
+      <h1 className={styles.title}>Welcome back!!!</h1>
+      <Button text={"Continue"} />
+    </div>
   );
 }
-
-export default App;
