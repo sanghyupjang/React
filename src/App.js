da60ef8212a1cbfa8b8827597156c56ac109d52a
Sanghyup <sanghyeob93@naver.com>
2022-02-16 01:13:48 +0900

    push

--- src/App.js
+++ src/App.js
@@ -2,17 +2,32 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-    console.log("i run all the time");
-    useEffect(() => {
-      console.log("CALL THE API....");
-    }, []);
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() => {
+    console.log("I run only once.");
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when keyword & counter change");
+  }, [keyword, counter]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
-      <button className={onclick}>click me</button>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
-
 export default App;
