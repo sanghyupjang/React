eb536a3c0fcc92bba4d9247d14eedc80ea148cf1
Sanghyup <sanghyeob93@naver.com>
2022-02-20 15:14:23 +0900

    push

--- src/App.js
+++ src/App.js
@@ -12,9 +12,11 @@ function App() {
     setToDos((currentArray) => [toDo, ...currentArray]);
     setToDo("");
   };
+  console.log(toDos);
+  console.log(toDos.map((item, index) => <li key={index}>{item}</li>));
   return (
     <div>
-    <h1>My To Dos ({toDos.length})</h1>
+      <h1>My To Dos ({toDos.length})</h1>
       <form onSubmit={onSubmit}>
         <input
           onChange={onChange}
@@ -24,8 +26,13 @@ function App() {
         />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
-}    
-
+}
 export default App;
